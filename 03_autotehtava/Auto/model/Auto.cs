bd8e98403b86e500c4dc98a8389cef729dbbbebd
miskamero <miskamero@users.noreply.github.com>
2024-11-14 12:14:38 +0200

    t3.10 done

--- 03_autotehtava/Auto/model/Auto.cs
+++ 03_autotehtava/Auto/model/Auto.cs
@@ -8,6 +8,14 @@ namespace Autokauppa.model
 {
     public class Auto
     {
-
+        public int ID { get; set; }
+        public decimal Hinta { get; set; }
+        public DateTime Rekisteri_paivamaara { get; set; }
+        public decimal Moottorin_tilavuus { get; set; }
+        public int Mittarilukema { get; set; }
+        public int AutonMerkkiID { get; set; }
+        public int AutonMalliID { get; set; }
+        public int VaritID { get; set; }
+        public int PolttoaineID { get; set; }
     }
 }
